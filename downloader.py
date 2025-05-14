e34894d970fef5b05121e22f7c8813cdded3cbd8
J-Saeedi <j.saeedi.edu@gmail.com>
2025-05-14 10:26:15 +0330

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -49,39 +49,42 @@ def main(subreddit_name="MemeVideos"):
     conn, cursor = initialize_db()
 
     # Reddit API URL for fetching top posts from a subreddit
-    url = f"https://www.reddit.com/r/{subreddit_name}/top.json?t=all&limit=1000"
+    url = f"https://www.reddit.com/r/{subreddit_name}/top.json?t=all&limit=100"
 
     headers = {'User-Agent': 'video-downloader-script'}
-
+    after = ""
     try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()
-        data = response.json()
+        while 1:
+            response = requests.get(url+f"&after={after}", headers=headers)
+            response.raise_for_status()
+            data = response.json()
 
-        for post in data['data']['children']:
-            post_data = post['data']
+            for post in data['data']['children']:
+                post_data = post['data']
 
-            if 'is_video' in post_data and post_data['is_video']:
-                title = post_data['title']
-                video_url = post_data['url']
+                if 'is_video' in post_data and post_data['is_video']:
+                    title = post_data['title']
+                    video_url = post_data['url']
 
-                # Check if the video URL is already in the database
-                if is_url_in_db(cursor, video_url):
-                    print(f"Video already downloaded: {title}")
-                    continue
+                    # Check if the video URL is already in the database
+                    if is_url_in_db(cursor, video_url):
+                        print(f"Video already downloaded: {title}")
+                        continue
 
-                # Download the video using the provided function
-                download(video_url)
+                    # Download the video using the provided function
+                    download(video_url)
 
-                # Retrieve the file name from the download object
-                # file_name = reddit.file_name
-                file_name = os.path.basename(reddit.file_name)
-                # file_path = os.path.join("videos", file_name)
+                    # Retrieve the file name from the download object
+                    # file_name = reddit.file_name
+                    file_name = os.path.basename(reddit.file_name)
+                    # file_path = os.path.join("videos", file_name)
 
-                # Save video info to DB
-                save_video_info(
-                    cursor, title, os.path.basename(file_name), video_url)
-                conn.commit()
+                    # Save video info to DB
+                    save_video_info(
+                        cursor, title, os.path.basename(file_name), video_url)
+                    conn.commit()
+            after = data["data"]["after"]
+            print(f"going to after={after}")
 
     except requests.exceptions.HTTPError as err:
         print(f"HTTP error occurred: {err}")
